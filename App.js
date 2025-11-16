13b88105e587e2a42f723aa7f8b048be1cba9999
Manager <manager.companyy92@gmail.com>
2025-11-16 06:27:46 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,25 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { View, Text, StyleSheet } from 'react-native';
+import Navigation from './navigation';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Madera Design App</Text>
-      <StatusBar style="light" />
-    </View>
-  );
+  return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#2B2B2B',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#D4B89B',
-    fontSize: 24,
-    fontWeight: 'bold',
-  }
-});
